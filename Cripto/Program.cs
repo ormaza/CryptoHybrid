c787dca0653e434bb92af3629e0ee6b6265a1247
ormazabal <ormazabalnascimento@gmail.com>
2023-05-27 00:27:45 -0300

    criptografia burra

--- Cripto/Program.cs
+++ Cripto/Program.cs
@@ -20,6 +20,14 @@ namespace Cripto
 
             Console.WriteLine("encriptado: " + encriptada);
             Console.WriteLine("decriptada: " + decriptada);
+
+            DateTime date = DateTime.Now;
+            int shift = (date.Year % date.Month) + (date.Day/date.Month);
+            encriptada = PoorEncrypt("ormazabal lima do nascimento", shift);
+            decriptada = PoorDecrypt(encriptada, shift);
+
+            Console.WriteLine("encriptado: " + encriptada);
+            Console.WriteLine("decriptada: " + decriptada);
         }
 
         static string Decrypt(Entrada entrada)
@@ -101,6 +109,50 @@ namespace Cripto
                 byte[] plainText = Encoding.UTF8.GetBytes(entrada.texto);
 
                 return Convert.ToBase64String(transform.TransformFinalBlock(plainText, 0, plainText.Length));
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        static string PoorEncrypt(string texto, int shift)
+        {
+            try
+            {
+                Random random = new Random();
+                string nova = "";
+                for (int i = 0; i < texto.Length; i++)
+                {
+                    nova += (char) (texto[i] + shift);
+                    nova += (char)(random.Next(65, 122 + 1) % 255);
+                    nova += (char)(random.Next(65, 122 + 1) % 255);
+                }
+                return "ios" + nova;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        static string PoorDecrypt(string texto, int shift)
+        {
+            try
+            {
+                if (texto.Substring(0, 3) == "ios")
+                {
+                    string texto2;
+                    texto2 = texto.Substring(3);
+                    string nova = "";
+                    for (int i = 0; i < texto2.Length; i+=3)
+                    {
+                        nova += (char)(texto2[i] - shift);
+                    }
+                    return nova;
+                }
+                else
+                    return "nao tem ios";
             }
             catch (Exception ex)
             {
